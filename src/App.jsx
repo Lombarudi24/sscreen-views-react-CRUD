2f4ffe6addb588935f7c8f18283b0d53cd255cb3
Rubby <rubenslombard@pursuit.org>
2023-08-11 12:28:21 -0400

    added env variable

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import {BrowserRouter as Router} from "react-router-dom"
 import './App.css'
 
 function App() {
- 
+  const var10_1 = import.meta.env.VITE_10_1_VAR;
+  console.log("show the environment variable", var10_1 ) 
   return (
     <>
       <Router>
         <h1>ScreenViews</h1>
+
+        <h2>-TEST-{var10_1 }</h2>
       </Router>
     </>
   );
